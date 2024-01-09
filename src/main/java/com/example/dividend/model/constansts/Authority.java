be4a7abd747c0791c1706b24fd3d3ffa1e1c81ef
itpark1018 <itpark1018@gmail.com>
2024-01-09 21:58:32 +0900

    배당금

---
+++ src/main/java/com/example/dividend/model/constansts/Authority.java
@@ -0,0 +1,6 @@
+package com.example.dividend.model.constansts;
+
+public enum Authority {
+    ROLE_READ,
+    ROLE_WRITE;
+}
---
+++ src/main/java/com/example/dividend/scraper/NaverFinanceScraper.java
@@ -0,0 +1,16 @@
+package com.example.dividend.scraper;
+
+import com.example.dividend.model.Company;
+import com.example.dividend.model.ScrapeResult;
+
+public class NaverFinanceScraper implements Scraper{
+    @Override
+    public Company scrapCompanyByTicker(String ticker) {
+        return null;
+    }
+
+    @Override
+    public ScrapeResult scrap(Company company) {
+        return null;
+    }
+}
